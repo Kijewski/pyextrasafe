7003dc1d2c9715cd094b03ac240978bfea51b87e
René Kijewski <rene.kijewski@fu-berlin.de>
2023-05-22 22:49:18 +0200

    Add project_urls

--- setup.py
+++ setup.py
@@ -6,14 +6,15 @@ from setuptools import setup
 from setuptools_rust import RustExtension, Strip
 
 
-setup(
-    rust_extensions=[
-        RustExtension(
-            "pyextrasafe._pyextrasafe",
-            debug=False,
-            strip=Strip.Debug,
-            features=[f"pyo3/abi3-py{version_info[0]}{version_info[1]}"],
-            py_limited_api=True,
-        )
-    ],
-)
+if __name__ == "__main__":
+    setup(
+        rust_extensions=[
+            RustExtension(
+                "pyextrasafe._pyextrasafe",
+                debug=False,
+                strip=Strip.Debug,
+                features=[f"pyo3/abi3-py{version_info[0]}{version_info[1]}"],
+                py_limited_api=True,
+            )
+        ],
+    )
